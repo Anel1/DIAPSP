87770587c18df487cd181d6e2901ae6c1fcf08a7
Anel <anel@dataintoprofit.com>
2015-12-09 14:17:29 +0200

    Adding Project to GitHub
    Adding DIAPSP Project to GitHub

---
+++ app/src/main/java/com/dataintoprofit/diapsp/VisitDetails.java
@@ -0,0 +1,14 @@
+package com.dataintoprofit.diapsp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VisitDetails extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit_details);
+    }
+}
